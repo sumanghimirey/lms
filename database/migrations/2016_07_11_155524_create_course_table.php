026ac5e2225830e7463a5a98e300daeda2086929
sumanghimirey <working.forum7@gmail.com>
2016-07-11 22:03:03 +0545

    inital commit

---
+++ database/migrations/2016_07_11_155524_create_course_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCourseTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('course', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('institute');
+            $table->string('teacher');
+            $table->string('category');
+            $table->integer('series_id')->unsigned();
+            $table->foreign('series_id')->references('id')
+                ->on('series')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+
+            $table->integer('episode_id')->unsigned();
+            $table->foreign('episode_id')->references('id')
+                ->on('episode')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('course');
+    }
+}
